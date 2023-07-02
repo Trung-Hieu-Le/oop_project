c3e387b08f3048fe1991cda3d4bead6313a2b921
Trung-Hieu-Le <Trung-Hieu-Le@users.noreply.github.com>
2023-07-02 10:45:17 +0700

    update pattern

---
+++ src/main/java/com/home_project/oop_project/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.home_project.oop_project.exception;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
